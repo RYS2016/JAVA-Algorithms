442df5a5336626bc416ae088b6689b133baf9b0b
Yaroslav <rykal2014@gmail.com>
2018-11-24 10:25:00 -0500

    Create OddNumbers.java

---
+++ OddNumbers.java
@@ -0,0 +1,9 @@
+    static String findNumber(List<Integer> arr, int k) {
+
+            for(int e : arr) { 
+                if (e == k) { 
+                    return "Yes"; 
+                    } 
+                }
+            return "NO";
+    }
